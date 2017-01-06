a64082ecfbe32610e5d2d5a51b652d1752dfacc2
Haisam880 <Haisam880@gmail.com>
2017-01-07 02:41:59 +0500

    first commit

---
+++ HealthGui/app/src/main/java/com/haisamrasool/healthgui/MainActivity.java
@@ -0,0 +1,26 @@
+package com.haisamrasool.healthgui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import static com.haisamrasool.healthgui.R.id.editText;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText yourEditText= (EditText) findViewById(R.id.editText);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(yourEditText, InputMethodManager.SHOW_IMPLICIT);
+        yourEditText.requestFocus();
+
+    }
+}
